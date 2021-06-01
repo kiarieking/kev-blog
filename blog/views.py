7f504684ff11277f7c4eb1828189b64508a592ae
kevin <kiariekevin22@gmail.com>
2021-06-01 05:12:02 +0300

    post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Post
 
 
@@ -6,3 +6,9 @@ def post_list(request):
     posts = Post.objects.all()
     context = {'posts':posts}
     return render(request, 'blog/post_list.html', context)
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post,pk=id)
+    context = {'post': post}
+    return render(request, 'blog/post_detail.html', context)
