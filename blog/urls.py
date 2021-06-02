d65c8f5b71d1b827fc71125f7cd50215aa74a8a0
kevin <kiariekevin22@gmail.com>
2021-06-02 15:31:04 +0300

    create and edit post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('<int:id>/post_detail', views.post_detail, name='post_detail')
+    path('<int:id>/post_detail', views.post_detail, name='post_detail'),
+    path('post_new', views.post_new, name='post_new'),
+    path('<int:id>/edit', views.post_edit, name='post_edit')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Post
-
+from . forms import PostForm
+from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.all()
@@ -9,6 +10,37 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    post = get_object_or_404(Post,pk=id)
+    post = get_object_or_404(Post, pk=id)
     context = {'post': post}
     return render(request, 'blog/post_detail.html', context)
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.id)
+    else:
+        form = PostForm()
+    context = {'form':form}
+    return render(request, 'blog/post_edit.html', context)
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, pk=id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', post.id)
+    else:
+        form = PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context)
